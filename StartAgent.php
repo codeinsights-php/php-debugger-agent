53703f3ece6e8c71c0c430947cb5df9efd1cc19d
Juris <juris@d3.lv>
2022-11-03 14:09:02 +0200

    Initial version

---
+++ StartAgent.php
@@ -0,0 +1,66 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+// Load configuration and secrets from .env file
+$dotenv = Dotenv\Dotenv::createMutable(__DIR__);
+$dotenv->load();
+
+$webSocketsClient = new \CodeInsights\Debugger\Agent\WebSocketsClient();
+
+$loop = \React\EventLoop\Factory::create();
+
+$loop->addPeriodicTimer(1, function () use ($webSocketsClient) {
+    $webSocketsClient->uploadSnapshots();
+});
+
+$connectionOptions = [
+    'timeout' => 3,
+];
+
+if (empty($_ENV['CODEINSIGHTS_MESSAGING_SERVER_USE_SPECIFIC_DNS_SERVER']) === false)
+{
+    $connectionOptions['dns'] = $_ENV['CODEINSIGHTS_MESSAGING_SERVER_USE_SPECIFIC_DNS_SERVER'];
+}
+
+$reactConnector = new \React\Socket\Connector($loop, $connectionOptions);
+
+$connector = new \Ratchet\Client\Connector($loop, $reactConnector);
+
+$messagingServerEndpoint = 'wss://' . $_ENV['CODEINSIGHTS_MESSAGING_SERVER_HOST'] .'/app/' . $_ENV['CODEINSIGHTS_MESSAGING_SERVER_KEY'] . '?protocol=7&flash=false';
+
+// https://pusher.com/docs/channels/library_auth_reference/pusher-websockets-protocol/
+$connector($messagingServerEndpoint)->then(function ($connection) use ($webSocketsClient) {
+    $connection->on('message', function ($msg) use ($connection, $webSocketsClient) {
+        echo "Received: {$msg}\n";
+
+        $message = json_decode($msg, false);
+
+        if (is_string($message->data)) {
+            // event "pusher:connection_established" has data as string which contains json
+            $message->data = json_decode($message->data, false);
+        }
+
+        print_r($message);
+
+        $response = $webSocketsClient->handleIncomingMessage($message);
+
+        if (empty($response) === false) {
+            $connection->send(json_encode($response));
+        }
+    });
+
+    // Custom addition to Ratchet code has to be added
+    // as $this->emit('response-sent', [$msg, $this]);
+    // in src/WebSocket.php
+    $connection->on('response-sent', function ($msg) {
+        echo "Sent: {$msg}\n";
+    });
+
+// TODO: On "close" event handler?
+}, function (\Exception $e) use ($loop) {
+    echo "Could not connect: {$e->getMessage()}\n";
+    $loop->stop();
+});
+
+$loop->run();
---
+++ src/WebSocketsClient.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace CodeInsights\Debugger\Agent;
+
+use Exception, stdClass;
+
+class WebSocketsClient {
+
+    public function handleIncomingMessage(stdClass $message) : array
+    {
+        if (isset($message->event) === false)
+        {
+            d('Invalid message received.');
+            return [];
+        }
+
+        switch ($message->event)
+        {
+            case 'pusher:connection_established';
+                return $this->handleCommandConnectionEstablished($message);
+            case 'pusher_internal:subscription_succeeded';
+                // Ignoring
+                return [];
+            // TODO:
+            case 'client-my-manual-event':
+                return $this->handleCommandCustom($message);
+        }
+
+        d('Unknown command received.');
+
+        return [];
+    }
+
+    private function handleCommandConnectionEstablished(stdClass $message) : array
+    {
+        $socket_id = $message->data->socket_id;
+
+        // TODO: Obtain authentication signature from external service (which validates if access to the channel can be granted?) (or just knows / gives the channel name)
+        $authentication_signature = hash_hmac('sha256', $socket_id . ':' . $_ENV['CODEINSIGHTS_MESSAGING_SERVER_CHANNEL'], $_ENV['CODEINSIGHTS_MESSAGING_SERVER_SECRET']);
+
+        return [
+            'event' => 'pusher:subscribe',
+            'data' => [
+                'auth' => $_ENV['CODEINSIGHTS_MESSAGING_SERVER_KEY'] . ':' . $authentication_signature,
+                'channel' => $_ENV['CODEINSIGHTS_MESSAGING_SERVER_CHANNEL'],
+            ]
+        ];
+    }
+
+    private function handleCommandCustom(stdClass $message) : array
+    {
+        return [
+            'event' => 'client-my-manual-event-response',
+            'channel' => $_ENV['CODEINSIGHTS_MESSAGING_SERVER_CHANNEL'],
+            'data' => [
+                'Some random gibberish in response: ' . time(),
+            ],
+        ];
+    }
+
+    public function uploadSnapshots() : void
+    {
+        // TODO
+        // d('Checking for new dumps to upload.');
+        // TODO: Ping pong
+        // TODO: And if haven't heard back - disable debugging
+    }
+
+}
+
+function d(mixed $variable = '')
+{
+    echo $variable . "\n";
+}
