93a9064392ee13216685ea87feaf5dcb745c5fdf
Juris <juris@d3.lv>
2023-06-05 17:11:25 +0300

    Test to verify that logpoint removal commands are being processed

--- tests/E2E/IntegrationWithCoreWebSocketsServerTest.php
+++ tests/E2E/IntegrationWithCoreWebSocketsServerTest.php
@@ -68,9 +68,57 @@ debug_helper
 \\CodeInsights\\Debugger\\Helper::debug(\'\', \'\', get_defined_vars(), debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS), __FILE__, __LINE__);
 ');
 
+   $this->webSocketsClientUser->closeConnection();
+
 });
 
-// TODO: Test logpoint removal
+it('removes logpoints', function () {
+
+   $this->webSocketsClientUser->handleAuthenticationMessagesFirst();
+
+   $file_path = '/data/www/laravel-sample-project/app/Http/Controllers/NewsletterController.php';
+
+   $this->webSocketsClientUser->sendMessage([
+      'event' => 'logpoint-add',
+      'data' => [
+         'file_path' => $file_path,
+         'file_hash' => '489ffad6',
+         'line_number' => 18,
+      ],
+   ]);
+
+   // Read data from confirmation that logpoint is propagated across servers
+   $message = json_decode($this->webSocketsClientUser->receiveMessage(), true);
+
+   $logpoint_id = $message['data']['logpoint_id'];
+
+   // Skip confirmation that logpoint was added
+   $this->webSocketsClientUser->receiveMessage();
+
+   $this->webSocketsClientUser->sendMessage([
+      'event' => 'logpoint-remove',
+      'data' => [
+         'logpoint_id' => $logpoint_id,
+      ],
+   ]);
+
+   $message = json_decode($this->webSocketsClientUser->receiveMessage(), true);
+
+   expect($message)->toMatchArray([
+      'event' => 'logpoint-removal-pending',
+      'data' => [
+         'logpoint_id' => $logpoint_id,
+         'removal_reason_code' => 101,
+         'removal_reason_message' => 'Logpoint removed by a user',
+      ],
+   ]);
+
+   // Verify that Agent wrote the configuration file
+   $logpointsConfiguration = file_get_contents(ini_get('codeinsights.directory') . ini_get('codeinsights.breakpoint_file'));
+
+   expect($logpointsConfiguration)->toBe('version=1' . "\n");
+
+});
 
 // TODO: Add test for failing to add logpoint - file does not exist
 // TODO: Add test for failing to add logpoint - file hash mismatch
